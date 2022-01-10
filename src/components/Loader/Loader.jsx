b08a6758ad8dfdcbd477df0dad455147235ead77
SoftTouchSys <izaid57@gmail.com>
2022-01-10 12:06:50 +0500

    updated

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./Loader.css";
+
+const Loader = () => {
+
+  return (
+    <Fragment>
+
+      <div className="loader"></div>
+
+    </Fragment>
+  );
+};
+
+export default Loader;
